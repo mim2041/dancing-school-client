14a31d736d3d5992b1820eee6c1869bb3e051e36
mim2041 <suraiyamim338@gmail.com>
2023-06-08 19:22:53 +0600

    Navbar added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,37 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Classes from "../pages/Classes/Classes/Classes";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/Login/SignUp";
+import Instructors from "../pages/Instructors/Instructors/Instructors";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: 'instructors',
+          element: <Instructors></Instructors>
+        },
+        {
+          path: 'classes',
+          element: <Classes></Classes>
+        },
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <SignUp></SignUp>
+        }
+      ]
     },
   ]);
 
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,9 +1,36 @@
-
+import { Link } from "react-router-dom";
+import logo1 from '../../assets/images/logo1.jpg'
 
 const Navbar = () => {
+    const navItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/instructors'>Instructors</Link></li>
+        <li><Link to='/classes'>Classes</Link></li>
+        <li><Link to='/login'>login</Link></li>
+                    
+    </>
+
     return (
-        <div>
-            
+        <div className="navbar bg-green-100 py-8 lg:justify-between fixed z-10 max-w-screen-xl bg-opacity-30">
+            <div className="navbar-start">
+                <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    {navItems}
+                </ul>
+                </div>
+                <div>
+                    <img src={logo1} width={"50px"} className="mx-auto rounded-lg" alt="" />
+                    <a className="btn btn-ghost normal-case text-xl">Dancing School</a>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                {navItems}
+                </ul>
+            </div>
         </div>
     );
 };
